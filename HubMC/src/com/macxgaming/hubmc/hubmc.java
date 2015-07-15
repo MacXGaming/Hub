235181b17fe1a6e2751c3e3b9c15924d8c5adaef
René Dyhr <bazzo39@gmail.com>
2015-07-15 16:32:27 +0200

    Cleaning

--- HubMC/src/com/macxgaming/hubmc/hubmc.java
+++ HubMC/src/com/macxgaming/hubmc/hubmc.java
@@ -18,90 +18,88 @@ import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class hubmc extends JavaPlugin implements Listener {
-  public void onEnable() {
-    saveDefaultConfig();
-    Bukkit.getPluginManager().registerEvents(this, this);
-  }
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent e) {
-    if (getConfig().getBoolean("forcespawn")) {
-      Player p = e.getPlayer();
-      if (getConfig().getConfigurationSection("spawn") == null) {
-        p.sendMessage(ChatColor.RED + "The spawn has not yet been set!");
-      }else{
-        World w = Bukkit.getServer().getWorld(getConfig().getString("spawn.world"));
-        double x = getConfig().getDouble("spawn.x");
-        double y = getConfig().getDouble("spawn.y");
-        double z = getConfig().getDouble("spawn.z");
-        float yaw = (float)getConfig().getDouble("spawn.yaw");
-        float pitch = (float)getConfig().getDouble("spawn.pitch");
-        p.teleport(new Location(w, x, y, z, yaw, pitch));
-      }
-    }
-  }
+	public void onEnable() {
+	  saveDefaultConfig();
+	  Bukkit.getPluginManager().registerEvents(this, this);
+	}
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		if (getConfig().getBoolean("forcespawn")) {
+			Player p = e.getPlayer();
+			if (getConfig().getConfigurationSection("spawn") == null) {
+				p.sendMessage(ChatColor.RED + "The spawn has not yet been set!");
+			}else{
+				World w = Bukkit.getServer().getWorld(getConfig().getString("spawn.world"));
+				double x = getConfig().getDouble("spawn.x");
+				double y = getConfig().getDouble("spawn.y");
+				double z = getConfig().getDouble("spawn.z");
+				float yaw = (float)getConfig().getDouble("spawn.yaw");
+				float pitch = (float)getConfig().getDouble("spawn.pitch");
+				p.teleport(new Location(w, x, y, z, yaw, pitch));
+			}
+		}
+	}
   
-  public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-    Player p = (Player)sender;
-    if (cmd.getName().equalsIgnoreCase("hsetspawn")) {
-    	if(p.isOp()){
-			getConfig().set("spawn.world", p.getLocation().getWorld().getName());
-			getConfig().set("spawn.x", Double.valueOf(p.getLocation().getX()));
-			getConfig().set("spawn.y", Double.valueOf(p.getLocation().getY()));
-			getConfig().set("spawn.z", Double.valueOf(p.getLocation().getZ()));
-			getConfig().set("spawn.yaw", Float.valueOf(p.getLocation().getYaw()));
-			getConfig().set("spawn.pitch", Float.valueOf(p.getLocation().getPitch()));
-			saveConfig();
-			p.sendMessage(ChatColor.GREEN + "Spawn set!");
-			getLogger().info("Hub Spawn set!");
-    	}else{
-    		p.sendMessage("&cYou dont have the permission");
-    	}
-    }
-    if(cmd.getName().equalsIgnoreCase("hreload")) {
-      if(p.isOp()){
-        saveConfig();
-        reloadConfig();
-        p.sendMessage(ChatColor.GREEN + "Reloaded the config.yml");
-        getLogger().info("Reloaded the config.yml");
-        return true;
-      }
-      p.sendMessage("&cYou dont have the permission");
-      return true;
-    }
-    return true;
-  }
-  
-  @EventHandler(priority=EventPriority.HIGHEST)
-  public void onCommand(PlayerCommandPreprocessEvent event) {
-    String command = event.getMessage();
-    for (int i = 0; i < getConfig().getList("blocked-cmds").size(); i++) {
-      String playercommand = (String)getConfig().getList("blocked-cmds").get(i);
-      if (command.toUpperCase().contains("/" + playercommand.toUpperCase())) {
-        Player p = event.getPlayer();
-        p.sendMessage(getConfig().getString("blocked-cmd-message").replaceAll("&", "§"));
-        event.setCancelled(true);
-      }
-    }
-  }
-  
-  @EventHandler
-  public void onPlayerDropItem(PlayerDropItemEvent event) {
-    if (getConfig().getBoolean("nodrop")) {
-      event.setCancelled(true);
-    }
-  }
-  @EventHandler(priority=EventPriority.HIGHEST)
-  public void rain(WeatherChangeEvent e) {
-    if ((getConfig().getBoolean("weather")) && 
-      (e.toWeatherState())) {
-      List<String> worlds = getConfig().getStringList("worlds");
-      for (String w : worlds) {
-        World world = Bukkit.getServer().getWorld(w);
-        if (e.getWorld().equals(world)) {
-          e.setCancelled(true);
-          world.setStorm(false);
-        }
-      }
-    }
-  }
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		Player p = (Player)sender;
+		if (cmd.getName().equalsIgnoreCase("hsetspawn")) {
+			if(p.isOp()){
+				getConfig().set("spawn.world", p.getLocation().getWorld().getName());
+				getConfig().set("spawn.x", Double.valueOf(p.getLocation().getX()));
+				getConfig().set("spawn.y", Double.valueOf(p.getLocation().getY()));
+				getConfig().set("spawn.z", Double.valueOf(p.getLocation().getZ()));
+				getConfig().set("spawn.yaw", Float.valueOf(p.getLocation().getYaw()));
+				getConfig().set("spawn.pitch", Float.valueOf(p.getLocation().getPitch()));
+				saveConfig();
+				p.sendMessage(ChatColor.GREEN + "Spawn set!");
+				getLogger().info("Hub Spawn set!");
+			}else{
+				p.sendMessage("&cYou dont have the permission");
+			}
+		}
+		if(cmd.getName().equalsIgnoreCase("hreload")) {
+			if(p.isOp()){
+				saveConfig();
+				reloadConfig();
+				p.sendMessage(ChatColor.GREEN + "Reloaded the config.yml");
+				getLogger().info("Reloaded the config.yml");
+				return true;
+			}else{
+				p.sendMessage("&cYou dont have the permission");
+				return true;
+			}
+		}
+		return true;
+	}
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onCommand(PlayerCommandPreprocessEvent event) {
+		String command = event.getMessage();
+		for (int i = 0; i < getConfig().getList("blocked-cmds").size(); i++) {
+			String playercommand = (String)getConfig().getList("blocked-cmds").get(i);
+			if (command.toUpperCase().contains("/" + playercommand.toUpperCase())) {
+				Player p = event.getPlayer();
+				p.sendMessage(getConfig().getString("blocked-cmd-message").replaceAll("&", "§"));
+				event.setCancelled(true);
+			}
+		}
+	} 
+	@EventHandler
+	public void onPlayerDropItem(PlayerDropItemEvent event) {
+		if (getConfig().getBoolean("nodrop")) {
+			event.setCancelled(true);
+		}
+	}
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void rain(WeatherChangeEvent e) {
+		if ((getConfig().getBoolean("weather")) && (e.toWeatherState())) {
+			List<String> worlds = getConfig().getStringList("worlds");
+			for (String w : worlds) {
+				World world = Bukkit.getServer().getWorld(w);
+				if (e.getWorld().equals(world)) {
+					e.setCancelled(true);
+					world.setStorm(false);
+				}
+			}
+		}
+	}
 }
